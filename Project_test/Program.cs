dc5176cb860962413665c6af50b7b6ddc1d7a4a4
Kim Eroico <kim-eroico@hotmail.com>
2020-01-22 17:31:07 -0300

    Bom dia adicionado

--- Project_test/Program.cs
+++ Project_test/Program.cs
@@ -4,6 +4,7 @@ namespace Project_test {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
         }
     }
 }
