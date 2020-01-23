3fcf3923b7876ce97934504ff28bc24a1cf37b5b
Kim Eroico <kim-eroico@hotmail.com>
2020-01-23 08:09:41 -0300

    Seja bemvindo

--- Project_test/Program.cs
+++ Project_test/Program.cs
@@ -4,7 +4,8 @@ namespace Project_test {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Bom Dia 25!");
+            Console.WriteLine("Seja bem-vindo!!");
         }
     }
 }
